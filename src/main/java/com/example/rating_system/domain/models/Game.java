88778f0a701dd26fbe284c3aaa12028c98487fa7
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-14 13:51:50 +0400

    Add game domain model

---
+++ src/main/java/com/example/rating_system/domain/models/Game.java
@@ -0,0 +1,43 @@
+package com.example.rating_system.domain.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class Game {
+    private Integer id;
+    private String title;
+    private String description;
+    private ArrayList<GameObject> gameObjects;
+
+    @Builder
+    public Game(String title,
+                String description,
+                ArrayList<GameObject> gameObjects
+    ) {
+        if (title == null || title.isBlank()) throw new IllegalArgumentException("Title required");
+        this.title = title.trim();
+        this.description = description.trim();
+        this.gameObjects = gameObjects;
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.isBlank()) throw new IllegalArgumentException("Title required");
+        this.title = title.trim();
+    }
+
+    public void setDescription(String description) {
+        this.description = description.trim();
+    }
+
+    public void setGameObjects(ArrayList<GameObject> gameObjects) {
+        this.gameObjects = gameObjects != null ? gameObjects : new ArrayList<>();
+    }
+
+    public void addGameObject(GameObject obj) {
+        this.gameObjects.add(obj);
+    }
+
+}
