b798d2a71209f3b3a129b764d7d09394079156ed
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-14 14:11:16 +0400

    Add Rating domain model

---
+++ src/main/java/com/example/rating_system/domain/models/Rating.java
@@ -0,0 +1,49 @@
+package com.example.rating_system.domain.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Rating {
+    private Integer id;
+    private Short score;
+    private User author;
+    private User targetSeller;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    @Builder
+    public Rating(Short score,
+                  User author,
+                  User targetSeller,
+                  LocalDateTime createdAt,
+                  LocalDateTime updatedAt
+    ) {
+        setScore(score);
+        if (author == null) throw new IllegalArgumentException("Author required");
+        if (targetSeller == null) throw new IllegalArgumentException("Target Seller required");
+        this.author = author;
+        this.targetSeller = targetSeller;
+        this.createdAt = createdAt != null ? createdAt : LocalDateTime.now();
+        this.updatedAt = updatedAt != null ? updatedAt : this.createdAt;
+    }
+
+    public void setScore(Short score) {
+        if (score >= 1 && score <= 5) {
+            this.score = score;
+        } else throw new IllegalArgumentException("Score must be between 1 and 5");
+    }
+
+    public void setAuthor(User author) {
+        if (author == null) throw new IllegalArgumentException("Author cannot be null");
+        this.author = author;
+    }
+
+    public void setTargetSeller(User targetSeller) {
+        if (targetSeller == null) throw new IllegalArgumentException("Target Seller cannot be null");
+        this.targetSeller = targetSeller;
+    }
+}
