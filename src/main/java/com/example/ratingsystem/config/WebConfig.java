8271e042e495a56c79434ff6f7c61296ed314cd1
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-15 18:05:15 +0400

    add WebConfig Class

--- src/main/java/com/example/ratingsystem/config/WebConfig.java
+++ src/main/java/com/example/ratingsystem/config/WebConfig.java
@@ -1,12 +1,44 @@
 package com.example.ratingsystem.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.example.ratingsystem")
-public class WebConfig {
+@ComponentScan("com.example.ratingsystem.adapters.inbound")
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        MappingJackson2HttpMessageConverter json = new MappingJackson2HttpMessageConverter(mapper);
+        converters.add(json);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static")
+                .addResourceLocations("/static/");
+    }
+
+
 }
