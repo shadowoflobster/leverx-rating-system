c2165d5f5e145c8d05ef8ba8f3dd7aa022cc5c34
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-16 18:03:47 +0400

    Add User jpa Entity class

---
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/entities/UserEntity.java
@@ -0,0 +1,56 @@
+package com.example.ratingsystem.adapters.outbound.persistence.entities;
+
+import com.example.ratingsystem.domain.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@Table(name = "users", schema = "leverx-rating")
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String fistName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<GameObjectEntity> gameObjects;
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
+    private List<CommentEntity> givenComments;
+
+    @OneToMany(mappedBy = "target", fetch = FetchType.LAZY)
+    private List<CommentEntity> takenComments;
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
+    private List<RatingEntity> givenRating;
+
+    @OneToMany(mappedBy = "target", fetch = FetchType.LAZY)
+    private List<RatingEntity> takenRating;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+
+}
