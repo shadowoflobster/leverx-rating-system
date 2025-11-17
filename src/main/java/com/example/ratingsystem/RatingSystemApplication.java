e1e04d17fad2bacbdd493baabbcdcd4c4fc72da3
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-17 17:46:59 +0400

    Test database connection from main

--- src/main/java/com/example/ratingsystem/RatingSystemApplication.java
+++ src/main/java/com/example/ratingsystem/RatingSystemApplication.java
@@ -1,13 +1,26 @@
 package com.example.ratingsystem;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.ratingsystem.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 public class RatingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatingSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        DataSource ds = context.getBean(DataSource.class);
 
+        try (Connection conn = ds.getConnection()) {
+            System.out.println("Connected to the database successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        context.close();
+    }
 }
+
+
+
