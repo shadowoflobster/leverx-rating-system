a4d440af863d6644695eea5b8d51574b2aee1281
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-18 21:02:26 +0400

    Added findByFullName method in JpaUserRepository which uses JPQL to perform case-insensitive partial match on concatenated first and last name supports fidning users with full name

--- src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaUserRepository.java
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaUserRepository.java
@@ -2,6 +2,14 @@ package com.example.ratingsystem.adapters.outbound.persistence.repositories;
 
 import com.example.ratingsystem.adapters.outbound.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, Integer> {
+
+    @Query("SELECT u FROM UserEntity u " +
+            "WHERE LOWER(CONCAT(u.firstName, ' ' , u.lastName)) LIKE LOWER(CONCAT('%', : namePart, '%'))")
+    List<UserEntity> findByFullName(@Param("fullName") String fullName);
 }
