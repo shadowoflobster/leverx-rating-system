effe09dd16a1ee3c75436eb31031ad8ef6cb9a0d
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-18 14:15:57 +0400

    initialize comment request;

--- src/main/java/com/example/ratingsystem/adapters/inbound/DTOs/requests/CommentRequest.java
+++ src/main/java/com/example/ratingsystem/adapters/inbound/DTOs/requests/CommentRequest.java
@@ -1,8 +1,14 @@
 package com.example.ratingsystem.adapters.inbound.DTOs.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Value;
 
 @Value
 public class CommentRequest {
+    @NotBlank(message = "Message text is required")
+    String message;
+    Integer authorId;
+
+    Integer targetId;
 
 }
