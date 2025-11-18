e2fbe900f352fb981cbdae2ae88c9b89a6e8fefb
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-18 20:58:54 +0400

    Change return type from void to User

--- src/main/java/com/example/ratingsystem/application/ports/User/SaveUserPort.java
+++ src/main/java/com/example/ratingsystem/application/ports/User/SaveUserPort.java
@@ -3,5 +3,5 @@ package com.example.ratingsystem.application.ports.User;
 import com.example.ratingsystem.domain.models.User;
 
 public interface SaveUserPort {
-    void save(User user);
+    User save(User user);
 }
