8134aa74f82a076a0c245796f36e458b57d8ff74
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-20 00:53:48 +0400

    Changed from lombok value to lombok data

--- src/main/java/com/example/ratingsystem/adapters/inbound/DTOs/requests/UserRequest.java
+++ src/main/java/com/example/ratingsystem/adapters/inbound/DTOs/requests/UserRequest.java
@@ -3,23 +3,26 @@ package com.example.ratingsystem.adapters.inbound.DTOs.requests;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-
-@Value
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserRequest {
     @NotBlank(message = "First name is required")
-    String firstName;
+    private String firstName;
 
-    String lastName;
+    private String lastName;
+
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
-    String email;
+    private String email;
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters")
-    String password;
-    String role;
+    private String password;
 
-
+    private String role;
 }
