1fd44cd62ce91e3fcbfaf338b652ed0081974c33
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-20 00:55:54 +0400

    Simplify UserRole enum: remove dbValue field and constructor

--- src/main/java/com/example/ratingsystem/domain/enums/UserRole.java
+++ src/main/java/com/example/ratingsystem/domain/enums/UserRole.java
@@ -4,26 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum UserRole {
-    ADMINISTRATOR("Administrator"),
-    SELLER("Seller"),
-    USER("User");
-
-    private final String dbValue;
-
-    UserRole(String dbValue) {
-        this.dbValue = dbValue;
-    }
-
-    public String getDbValue() {
-        return dbValue;
-    }
-
-    public static UserRole fromDbValue(String dbValue) {
-        for (UserRole role : values()) {
-            if (role.getDbValue().equalsIgnoreCase(dbValue)) {
-                return role;
-            }
-        }
-        throw new IllegalArgumentException("Unknown role:" + dbValue);
-    }
+    Administrator,
+    Seller,
+    User
 }
