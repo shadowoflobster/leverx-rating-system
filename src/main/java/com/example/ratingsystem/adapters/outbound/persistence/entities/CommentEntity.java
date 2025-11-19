1d28e418d48073258c00f18d36e76eaaaad63994
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-19 23:43:38 +0400

    Add lombok annotations

--- src/main/java/com/example/ratingsystem/adapters/outbound/persistence/entities/CommentEntity.java
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/entities/CommentEntity.java
@@ -1,10 +1,18 @@
 package com.example.ratingsystem.adapters.outbound.persistence.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "comments")
 public class CommentEntity {
     @Id
