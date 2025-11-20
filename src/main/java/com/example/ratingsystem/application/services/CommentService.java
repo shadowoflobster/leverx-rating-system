3c566df7a2c37d0a524775ae347dd2344b577b45
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-20 20:05:55 +0400

    Refractor addComment service method

--- src/main/java/com/example/ratingsystem/application/services/CommentService.java
+++ src/main/java/com/example/ratingsystem/application/services/CommentService.java
@@ -1,7 +1,10 @@
 package com.example.ratingsystem.application.services;
 
 import com.example.ratingsystem.adapters.inbound.DTOs.requests.CommentRequest;
+import com.example.ratingsystem.adapters.inbound.DTOs.responses.CommentResponse;
+import com.example.ratingsystem.adapters.outbound.persistence.mappers.CommentMapper;
 import com.example.ratingsystem.application.ports.Comment.AddCommentPort;
+import com.example.ratingsystem.domain.models.Comment;
 import com.example.ratingsystem.domain.models.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +13,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CommentService {
     private AddCommentPort addCommentPort;
+    private final CommentMapper commentMapper;
 
-    public void addComment(CommentRequest commentRequest){
-        addCommentPort.add(commentRequest);
+    public CommentResponse addComment(CommentRequest commentRequest){
+        Comment commentDomain = addCommentPort.add(commentRequest);
+
+        return commentMapper.domainToResponse(commentDomain);
+
     }
 
 }
