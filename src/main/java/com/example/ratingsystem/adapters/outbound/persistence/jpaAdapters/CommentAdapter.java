39183d87a887b9ef5e0b9268cf55c1f37f840d11
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-21 17:56:57 +0400

    refractor: if same authenticated user tries to write more than one comment to target seller, instead of adding new one old will updaated
    overrided methods of LoadCommentPort and DeleteCommentPort

--- src/main/java/com/example/ratingsystem/adapters/outbound/persistence/jpaAdapters/CommentAdapter.java
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/jpaAdapters/CommentAdapter.java
@@ -7,13 +7,19 @@ import com.example.ratingsystem.adapters.outbound.persistence.mappers.CommentMap
 import com.example.ratingsystem.adapters.outbound.persistence.repositories.JpaCommentRepository;
 import com.example.ratingsystem.adapters.outbound.persistence.repositories.JpaUserRepository;
 import com.example.ratingsystem.application.ports.Comment.AddCommentPort;
+import com.example.ratingsystem.application.ports.Comment.DeleteCommentPort;
+import com.example.ratingsystem.application.ports.Comment.LoadCommentPort;
 import com.example.ratingsystem.domain.models.Comment;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Component
 @AllArgsConstructor
-public class CommentAdapter implements AddCommentPort {
+public class CommentAdapter implements AddCommentPort, LoadCommentPort, DeleteCommentPort {
     private JpaCommentRepository jpaCommentRepository;
     private JpaUserRepository jpaUserRepository;
     private CommentMapper commentMapper;
@@ -37,11 +43,76 @@ public class CommentAdapter implements AddCommentPort {
                     .orElseThrow(() -> new IllegalArgumentException("Author user not found"));
         }
 
-        CommentEntity entity = commentMapper.requestToEntity(commentRequest, author, target);
+        Optional<CommentEntity> existing =
+                jpaCommentRepository.findByAuthor_IdAndTarget_Id(
+                        commentRequest.getAuthorId(),
+                        commentRequest.getTargetId()
+                );
+
+        CommentEntity entity;
+
+        if (existing.isPresent()) {
+            entity = existing.get();
+            entity.setMessage(commentRequest.getMessage());
+            entity.setUpdatedAt(LocalDateTime.now());
+        } else {
+            entity = commentMapper.requestToEntity(commentRequest, author, target);
+        }
 
         CommentEntity saved = jpaCommentRepository.save(entity);
 
         return commentMapper.entityToDomain(saved);
     }
+
+    @Override
+    public Optional<Comment> load(Integer id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Comment id cannot be null");
+        }
+
+        CommentEntity commentEntity = jpaCommentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Comment entity was not found with id " + id));
+
+        return Optional.of(commentMapper.entityToDomain(commentEntity));
+    }
+
+    @Override
+    public Optional<Comment> loadByAuthorAndTargetId(Integer authorId, Integer targetId) {
+        if (targetId == null) {
+            throw new IllegalArgumentException("Target ID is required");
+        }
+        if (authorId == null) {
+            return Optional.empty();
+        }
+
+        return jpaCommentRepository
+                .findByAuthor_IdAndTarget_Id(authorId, targetId)
+                .map(commentMapper::entityToDomain);
+    }
+
+    @Override
+    public List<Comment> loadByTargetId(Integer targetId) {
+        if (targetId == null) {
+            throw new IllegalArgumentException("Target ID is required");
+        }
+
+        return jpaCommentRepository.findByTarget_Id(targetId).stream()
+                .map(commentMapper::entityToDomain)
+                .toList();
+    }
+
+    @Override
+    public void deleteCommentById(Integer id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Comment ID cannot be null");
+        }
+
+        if (!jpaCommentRepository.existsById(id)) {
+            throw new IllegalArgumentException("Comment not found");
+        }
+
+        jpaCommentRepository.deleteById(id);
+    }
+
 
 }
