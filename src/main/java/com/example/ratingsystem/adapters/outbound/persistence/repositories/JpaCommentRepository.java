4e72c7a5c7956e3daff18fc373a3cfd0b131837f
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 00:48:26 +0400

    Add custom methods

--- src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaCommentRepository.java
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaCommentRepository.java
@@ -3,5 +3,11 @@ package com.example.ratingsystem.adapters.outbound.persistence.repositories;
 import com.example.ratingsystem.adapters.outbound.persistence.entities.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JpaCommentRepository extends JpaRepository<CommentEntity, Integer> {
+    List<CommentEntity> findByAuthor_IdAndTarget_Id(Integer authorId, Integer targetId);
+
+    List<CommentEntity> findByTarget_Id(Integer targetId);
+
 }
