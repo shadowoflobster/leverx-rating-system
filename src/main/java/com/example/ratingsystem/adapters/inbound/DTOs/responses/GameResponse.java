523388e11d1509aa6fcb8c55f8f0cdefecda4540
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 00:47:45 +0400

    add GameResponse

---
+++ src/main/java/com/example/ratingsystem/adapters/inbound/DTOs/responses/GameResponse.java
@@ -0,0 +1,13 @@
+package com.example.ratingsystem.adapters.inbound.DTOs.responses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameResponse {
+    public Integer id;
+}
