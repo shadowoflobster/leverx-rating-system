d5cfddc0e17663a30414e0e725039707e7600244
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 00:40:18 +0400

    fix: change return type and argument type for add method

--- src/main/java/com/example/ratingsystem/application/ports/Rating/AddRatingPort.java
+++ src/main/java/com/example/ratingsystem/application/ports/Rating/AddRatingPort.java
@@ -1,8 +1,10 @@
 package com.example.ratingsystem.application.ports.Rating;
 
+import com.example.ratingsystem.adapters.inbound.DTOs.requests.RatingRequest;
+import com.example.ratingsystem.domain.models.Comment;
 import com.example.ratingsystem.domain.models.Rating;
 
 public interface AddRatingPort {
-    void add(Rating rating);
+    Rating add(RatingRequest ratingRequest);
 
 }
