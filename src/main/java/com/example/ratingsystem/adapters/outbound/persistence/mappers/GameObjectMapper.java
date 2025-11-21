e325dafb7095f480bf633817934c843210ee754a
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 00:46:21 +0400

    Add GameObjectMapper

---
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/mappers/GameObjectMapper.java
@@ -0,0 +1,64 @@
+package com.example.ratingsystem.adapters.outbound.persistence.mappers;
+
+import com.example.ratingsystem.adapters.inbound.DTOs.requests.GameObjectRequest;
+import com.example.ratingsystem.adapters.inbound.DTOs.responses.GameObjectResponse;
+import com.example.ratingsystem.adapters.outbound.persistence.entities.GameEntity;
+import com.example.ratingsystem.adapters.outbound.persistence.entities.GameObjectEntity;
+import com.example.ratingsystem.adapters.outbound.persistence.entities.UserEntity;
+import com.example.ratingsystem.domain.models.GameObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class GameObjectMapper {
+    private UserMapper userMapper;
+    private GameMapper gameMapper;
+
+    public GameObjectResponse domainToResponse(GameObject domain) {
+        GameObjectResponse response = new GameObjectResponse();
+
+        response.setTitle(domain.getTitle());
+        response.setDescription(domain.getDescription());
+        response.setUserId(domain.getUser().getId());
+        response.setGameTitle(domain.getGame().getTitle());
+
+        return response;
+    }
+
+    public GameObjectEntity requestToEntity(GameObjectRequest request, UserEntity user, GameEntity game) {
+        if (request == null) {
+            throw new IllegalArgumentException("GameObjectRequest cannot be null");
+        }
+
+        GameObjectEntity entity = new GameObjectEntity();
+        entity.setTitle(request.getTitle());
+        entity.setDescription(request.getDescription());
+        entity.setUser(user);
+        entity.setGame(game);
+        entity.setCreatedAt(LocalDateTime.now());
+        entity.setUpdatedAt(LocalDateTime.now());
+
+        return entity;
+    }
+
+    public GameObject entityToDomain(GameObjectEntity entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException("GameObjectEntity cannot be null");
+        }
+
+        GameObject gameObject = GameObject.builder()
+                .title(entity.getTitle())
+                .description(entity.getDescription())
+                .user(entity.getUser() != null ? userMapper.entityToDomain(entity.getUser()) : null)
+                .game(entity.getGame() != null ? gameMapper.entityToDomain(entity.getGame()) : null)
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+
+        gameObject.setId(entity.getId());
+
+        return gameObject;
+    }
+
+}
