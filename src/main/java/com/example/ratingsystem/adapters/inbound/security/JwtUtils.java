bcfedadc7a130da0eff94aad5213996b80c3a62a
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 22:09:11 +0400

    Add JwtUtils

---
+++ src/main/java/com/example/ratingsystem/adapters/inbound/security/JwtUtils.java
@@ -0,0 +1,53 @@
+package com.example.ratingsystem.adapters.inbound.security;
+
+import com.example.ratingsystem.domain.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private final String secret = System.getenv("RATING_SYSTEM_KEY");
+    private long expirationMs = 7200000;
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setId(String.valueOf(user.getId()))
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole())
+                .claim("name", user.getFullName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
+
