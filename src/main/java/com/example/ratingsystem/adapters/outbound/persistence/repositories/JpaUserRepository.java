1d9a73f92f9448e181b3434eaa9ba6eb44b4cf91
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 22:08:46 +0400

    Add findByEmail jpa method

--- src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaUserRepository.java
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaUserRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, Integer> {
 
     @Query("SELECT u FROM UserEntity u " +
             "WHERE LOWER(CONCAT(u.firstName || ' ' ||  u.lastName)) LIKE LOWER(:fullName)")
     List<UserEntity> findByFullName(@Param("fullName") String fullName);
+
+    Optional<UserEntity> findByEmail(String email);
 }
