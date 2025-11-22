be9a0f8c252e162dfff2f7b15fbabf638df3646e
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 22:09:03 +0400

    Add JwtAuthFilter

---
+++ src/main/java/com/example/ratingsystem/adapters/inbound/security/JwtAuthFilter.java
@@ -0,0 +1,61 @@
+package com.example.ratingsystem.adapters.inbound.security;
+
+import com.example.ratingsystem.application.services.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final CustomUserDetailsService userDetailsService;
+
+    public JwtAuthFilter(JwtUtils jwtUtils, CustomUserDetailsService uds) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsService = uds;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
+
+        System.out.println("DEBUG: Entering JwtAuthFilter for URI: " + request.getRequestURI());
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+        try {
+            if (jwtUtils.validate(token)) {
+                String username = jwtUtils.extractUsername(token);
+                UserDetails user = userDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(
+                                user, null, user.getAuthorities()
+                        );
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }else{
+                System.out.println("Couldnt validate token");
+            }
+        }catch (Exception e){
+            System.err.println("Authentication Error: " + e.getMessage());
+        }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
+
