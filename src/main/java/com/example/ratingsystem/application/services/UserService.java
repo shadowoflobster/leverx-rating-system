d2fa1d9087c67597271905c518f536cef73d82e9
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 22:12:45 +0400

    Add login method

--- src/main/java/com/example/ratingsystem/application/services/UserService.java
+++ src/main/java/com/example/ratingsystem/application/services/UserService.java
@@ -1,16 +1,36 @@
 package com.example.ratingsystem.application.services;
 
+import com.example.ratingsystem.adapters.inbound.DTOs.requests.LoginRequest;
 import com.example.ratingsystem.adapters.inbound.DTOs.requests.UserRequest;
+import com.example.ratingsystem.adapters.inbound.security.JwtUtils;
+import com.example.ratingsystem.application.ports.User.LoadUserPort;
 import com.example.ratingsystem.application.ports.User.SaveUserPort;
-import lombok.AllArgsConstructor;
+import com.example.ratingsystem.domain.models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
-    private SaveUserPort saveUserPort;
+    private final SaveUserPort saveUserPort;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
+    private final LoadUserPort loadUserPort;
 
-    public void registerUser(UserRequest request){
+    public String login(LoginRequest request) {
+        User user = loadUserPort.loadByEmail(request.getEmail());
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        return jwtUtils.generateToken(user);
+    }
+
+    public void registerUser(UserRequest request) {
         saveUserPort.save(request);
+
     }
 }
