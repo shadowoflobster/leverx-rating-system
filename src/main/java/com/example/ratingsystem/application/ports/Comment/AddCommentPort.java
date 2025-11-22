603b503de858cef1b262b8fd7079492691b3cb29
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-22 22:03:02 +0400

    changed argument types

--- src/main/java/com/example/ratingsystem/application/ports/Comment/AddCommentPort.java
+++ src/main/java/com/example/ratingsystem/application/ports/Comment/AddCommentPort.java
@@ -4,5 +4,5 @@ import com.example.ratingsystem.adapters.inbound.DTOs.requests.CommentRequest;
 import com.example.ratingsystem.domain.models.Comment;
 
 public interface AddCommentPort {
-    Comment add(CommentRequest commentRequest);
+    Comment add(Integer targetId, String authorEmail, CommentRequest request);
 }
