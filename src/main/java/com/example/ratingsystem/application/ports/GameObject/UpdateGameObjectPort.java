bd5c56a37a3ea39bc430a51ee17cae255c7289bd
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-24 02:08:09 +0400

    Add UpdateGameObjectPort

---
+++ src/main/java/com/example/ratingsystem/application/ports/GameObject/UpdateGameObjectPort.java
@@ -0,0 +1,8 @@
+package com.example.ratingsystem.application.ports.GameObject;
+
+import com.example.ratingsystem.adapters.inbound.DTOs.requests.GameObjectRequest;
+import com.example.ratingsystem.domain.models.GameObject;
+
+public interface UpdateGameObjectPort {
+    GameObject updateById(Integer id, GameObjectRequest gameObjectRequest);
+}
