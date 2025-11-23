928188c09fd9a7bf304248c051159221db8a0132
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-23 20:59:11 +0400

    Add admin service

---
+++ src/main/java/com/example/ratingsystem/application/services/AdminService.java
@@ -0,0 +1,55 @@
+package com.example.ratingsystem.application.services;
+
+import com.example.ratingsystem.adapters.inbound.DTOs.responses.UserResponse;
+import com.example.ratingsystem.adapters.outbound.persistence.mappers.UserMapper;
+import com.example.ratingsystem.application.ports.User.DeleteUserPort;
+import com.example.ratingsystem.application.ports.User.LoadUserPort;
+import com.example.ratingsystem.application.ports.User.SaveUserPort;
+import com.example.ratingsystem.domain.models.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class AdminService {
+    private final LoadUserPort loadUserPort;
+    private final SaveUserPort saveUserPort;
+    private final DeleteUserPort deleteUserPort;
+    private final UserMapper userMapper;
+    private final EmailService emailService;
+
+    public List<UserResponse> getPendingUsers() {
+        List<User> users = loadUserPort.loadPendingUsers();
+
+        return users.stream()
+                .map(userMapper::domainToResponse)
+                .toList();
+    }
+
+    public void approveUser(Integer id) {
+        User user = loadUserPort.loadById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        if (user.isApproved()) {
+            throw new IllegalArgumentException("User is already approved");
+        }
+
+        user.setApproved(true);
+        emailService.sendVerificationEmail(user.getEmail());
+
+        saveUserPort.save(user);
+    }
+
+    public void rejectUser(Integer id) {
+        User user = loadUserPort.loadById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        if (user.isApproved()) {
+            throw new IllegalArgumentException("User is already approved");
+        }
+
+        deleteUserPort.deleteById(id);
+    }
+}
