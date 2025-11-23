efab6fe11e7fc61fddaee60a4cafcd023550f9aa
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-23 21:09:40 +0400

    refractor

--- src/main/java/com/example/ratingsystem/application/ports/User/SaveUserPort.java
+++ src/main/java/com/example/ratingsystem/application/ports/User/SaveUserPort.java
@@ -1,8 +1,7 @@
 package com.example.ratingsystem.application.ports.User;
 
-import com.example.ratingsystem.adapters.inbound.DTOs.requests.UserRequest;
 import com.example.ratingsystem.domain.models.User;
 
 public interface SaveUserPort {
-    User save(UserRequest request);
+    User save(User user);
 }
