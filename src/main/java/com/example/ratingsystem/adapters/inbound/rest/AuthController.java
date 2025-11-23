9698b31d60e6725d7147ea829b5c3d4b4bf76880
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-23 21:02:38 +0400

    fixed response Entities

--- src/main/java/com/example/ratingsystem/adapters/inbound/rest/AuthController.java
+++ src/main/java/com/example/ratingsystem/adapters/inbound/rest/AuthController.java
@@ -3,24 +3,45 @@ package com.example.ratingsystem.adapters.inbound.rest;
 import com.example.ratingsystem.adapters.inbound.DTOs.requests.LoginRequest;
 import com.example.ratingsystem.adapters.inbound.DTOs.requests.UserRequest;
 import com.example.ratingsystem.adapters.inbound.DTOs.responses.LoginResponse;
+import com.example.ratingsystem.adapters.inbound.DTOs.responses.UserResponse;
+import com.example.ratingsystem.adapters.outbound.persistence.mappers.UserMapper;
 import com.example.ratingsystem.application.services.UserService;
+import com.example.ratingsystem.domain.models.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserRequest request) {
-        userService.registerUser(request);
-        return ResponseEntity.ok("User Registered successfully");
+        try {
+            User user = userService.registerUser(request);
+            UserResponse response = userMapper.domainToResponse(user);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(Map.of(
+                            "success", true,
+                            "data", response
+                    ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of(
+                            "success", false,
+                            "message", e.getMessage()
+                    ));
+        }
     }
 
     @PostMapping("/login")
