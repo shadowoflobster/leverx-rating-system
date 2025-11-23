646cf7327a2bc80d4a94b251c756833afbe0a2a5
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-23 20:57:17 +0400

    Add methods to find ratings by target/author id

--- src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaRatingRepository.java
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaRatingRepository.java
@@ -3,5 +3,11 @@ package com.example.ratingsystem.adapters.outbound.persistence.repositories;
 import com.example.ratingsystem.adapters.outbound.persistence.entities.RatingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface JpaRatingRepository extends JpaRepository<RatingEntity, Integer> {
+    Optional<RatingEntity> findByAuthor_IdAndTarget_Id(Integer authorId, Integer targetId);
+
+    List<RatingEntity> findByTargetId(Integer targetId);
 }
