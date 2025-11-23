c1701bb9c05c9bead0a6693dbeb30acfac23bf3a
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-24 02:02:22 +0400

    Add EmailService

---
+++ src/main/java/com/example/ratingsystem/application/services/EmailService.java
@@ -0,0 +1,44 @@
+package com.example.ratingsystem.application.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@AllArgsConstructor
+public class EmailService {
+    private JavaMailSender mailSender;
+    private RedisTemplate<String, String> redisTemplate;
+
+    public void sendVerificationEmail(String email){
+        String code = UUID.randomUUID().toString();
+
+        redisTemplate.opsForValue().set(email,code,24, TimeUnit.HOURS);
+
+        String subject = "Confirm your email";
+        String body = "Your verification code is: " + code + "It will expire in 24 hours.";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+
+    }
+    public boolean verifyCode(String email, String code) {
+        String cachedCode = redisTemplate.opsForValue().get(email);
+        if (cachedCode != null && cachedCode.equals(code)) {
+            redisTemplate.delete(email);
+            return true;
+        }
+        return false;
+    }
+
+
+}
