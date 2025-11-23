e6eb1411e6d4003f249bbd9159c69da6b3fcee00
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-24 02:06:17 +0400

    Add methods

--- src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaGameObjectRepository.java
+++ src/main/java/com/example/ratingsystem/adapters/outbound/persistence/repositories/JpaGameObjectRepository.java
@@ -3,5 +3,11 @@ package com.example.ratingsystem.adapters.outbound.persistence.repositories;
 import com.example.ratingsystem.adapters.outbound.persistence.entities.GameObjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JpaGameObjectRepository extends JpaRepository<GameObjectEntity, Integer> {
+    List<GameObjectEntity> findBySellerId(Integer sellerId);
+    List<GameObjectEntity> findByGameId(Integer gameId);
+    List<GameObjectEntity> findByNameContainingIgnoreCase(String name);
+    List<GameObjectEntity> findByApprovedFalse();
 }
