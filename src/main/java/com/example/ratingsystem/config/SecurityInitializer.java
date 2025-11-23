3e5fdc5670b968a131aa7138a84e9e88255d52d4
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-23 21:10:24 +0400

    Add SecurityInitializer

---
+++ src/main/java/com/example/ratingsystem/config/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.example.ratingsystem.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
