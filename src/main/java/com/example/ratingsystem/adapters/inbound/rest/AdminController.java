30002a0f33a80fe73878388e177ad8cea1b38473
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-24 20:12:45 +0400

    Add approveComment, getPendingComments Refractor

--- src/main/java/com/example/ratingsystem/adapters/inbound/rest/AdminController.java
+++ src/main/java/com/example/ratingsystem/adapters/inbound/rest/AdminController.java
@@ -1,11 +1,14 @@
 package com.example.ratingsystem.adapters.inbound.rest;
 
+import com.example.ratingsystem.adapters.inbound.DTOs.responses.UserResponse;
 import com.example.ratingsystem.application.services.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,12 +17,15 @@ import java.util.Map;
 public class AdminController {
     private final AdminService adminService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/pending-users")
     public ResponseEntity<Map<String, Object>> getPendingUsers() {
         try {
+            List<UserResponse> users = adminService.getPendingUsers();
+
             return ResponseEntity.ok(Map.of(
                     "success", true,
-                    "data", adminService.getPendingUsers()
+                    "data", users
             ));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -30,6 +36,40 @@ public class AdminController {
         }
     }
 
+    @GetMapping("/pending-objects")
+    public ResponseEntity<Map<String, Object>> getPendingObjects() {
+        try {
+            return ResponseEntity.ok(Map.of(
+                    "success", true,
+                    "data", adminService.getPendingObjects()
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of(
+                            "success", false,
+                            "message", e.getMessage()
+                    ));
+        }
+    }
+
+    @PostMapping("/approve-object/{objectId}")
+    public ResponseEntity<Map<String, Object>> approveObject(@PathVariable Integer objectId) {
+        try {
+            adminService.approveObject(objectId);
+            return ResponseEntity.ok(Map.of(
+                    "success", true,
+                    "message", "Object approved successfully!"
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of(
+                            "success", false,
+                            "message", e.getMessage()
+                    ));
+        }
+    }
+
+
     @PostMapping("/approve-user/{userId}")
     public ResponseEntity<Map<String, Object>> approveUser(@PathVariable Integer userId) {
         try {
@@ -37,6 +77,39 @@ public class AdminController {
             return ResponseEntity.ok(Map.of(
                     "success", true,
                     "message", "User approved successfully! Verification email sent."
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of(
+                            "success", false,
+                            "message", e.getMessage()
+                    ));
+        }
+    }
+
+    @GetMapping("/pending-comment")
+    public ResponseEntity<Map<String, Object>> getPendingComments() {
+        try {
+            return ResponseEntity.ok(Map.of(
+                    "success", true,
+                    "data", adminService.getPendingComments()
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of(
+                            "success", false,
+                            "message", e.getMessage()
+                    ));
+        }
+    }
+
+    @PostMapping("/approve-comment/{commentId}")
+    public ResponseEntity<Map<String, Object>> approveComment(@PathVariable Integer commentId) {
+        try {
+            adminService.approveComment(commentId);
+            return ResponseEntity.ok(Map.of(
+                    "success", true,
+                    "message", "Comment approved successfully!"
             ));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
