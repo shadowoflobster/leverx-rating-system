dd61af38c5d058467ac9e9363eafe9e829184045
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-24 20:13:19 +0400

    Add approveComment, getPendingComments, ApproveObjects

--- src/main/java/com/example/ratingsystem/application/services/AdminService.java
+++ src/main/java/com/example/ratingsystem/application/services/AdminService.java
@@ -1,13 +1,19 @@
 package com.example.ratingsystem.application.services;
 
+import com.example.ratingsystem.adapters.inbound.DTOs.responses.CommentResponse;
 import com.example.ratingsystem.adapters.inbound.DTOs.responses.GameObjectResponse;
 import com.example.ratingsystem.adapters.inbound.DTOs.responses.UserResponse;
+import com.example.ratingsystem.adapters.outbound.persistence.mappers.CommentMapper;
 import com.example.ratingsystem.adapters.outbound.persistence.mappers.GameObjectMapper;
 import com.example.ratingsystem.adapters.outbound.persistence.mappers.UserMapper;
+import com.example.ratingsystem.application.ports.Comment.LoadCommentPort;
+import com.example.ratingsystem.application.ports.Comment.UpdateCommentPort;
 import com.example.ratingsystem.application.ports.GameObject.LoadGameObjectPort;
+import com.example.ratingsystem.application.ports.GameObject.UpdateGameObjectPort;
 import com.example.ratingsystem.application.ports.User.DeleteUserPort;
 import com.example.ratingsystem.application.ports.User.LoadUserPort;
 import com.example.ratingsystem.application.ports.User.SaveUserPort;
+import com.example.ratingsystem.domain.models.Comment;
 import com.example.ratingsystem.domain.models.GameObject;
 import com.example.ratingsystem.domain.models.User;
 import lombok.AllArgsConstructor;
@@ -21,9 +27,13 @@ public class AdminService {
     private final LoadUserPort loadUserPort;
     private final SaveUserPort saveUserPort;
     private final LoadGameObjectPort loadGameObjectPort;
+    private final LoadCommentPort loadCommentPort;
+    private final UpdateCommentPort updateCommentPort;
+    private final UpdateGameObjectPort updateGameObjectPort;
     private final GameObjectMapper gameObjectMapper;
     private final DeleteUserPort deleteUserPort;
     private final UserMapper userMapper;
+    private final CommentMapper commentMapper;
     private final EmailService emailService;
 
     public List<UserResponse> getPendingUsers() {
@@ -42,18 +52,49 @@ public class AdminService {
                 .toList();
     }
 
+    public List<CommentResponse> getPendingComments() {
+        List<Comment> comments = loadCommentPort.loadPendingComments();
+        return comments.stream()
+                .map(commentMapper::domainToResponse)
+                .toList();
+    }
+
     public void approveUser(Integer id) {
         User user = loadUserPort.loadById(id)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
 
+        emailService.sendVerificationEmail(user.getEmail());
         if (user.isApproved()) {
-            throw new IllegalArgumentException("User is already approved");
+            throw new IllegalArgumentException("User is already approved, Resending code...");
         }
 
         user.setApproved(true);
-        emailService.sendVerificationEmail(user.getEmail());
 
         saveUserPort.save(user);
+    }
+
+    public void approveComment(Integer id) {
+        Comment comment = loadCommentPort.load(id)
+                .orElseThrow(() -> new IllegalArgumentException("No comment found"));
+        if (comment.isApproved()) {
+            throw new IllegalArgumentException("Comment is already approved");
+        }
+        comment.setApproved(true);
+
+        updateCommentPort.update(comment);
+    }
+
+    public void approveObject(Integer id) {
+        GameObject object = loadGameObjectPort.loadById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Object not found"));
+
+        if (object.isApproved()) {
+            throw new IllegalArgumentException("Game Object is already approved");
+        }
+
+        object.setApproved(true);
+
+        updateGameObjectPort.update(object);
     }
 
     public void rejectUser(Integer id) {
