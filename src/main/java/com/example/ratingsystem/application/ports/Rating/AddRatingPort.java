a2203d634207323814301d7ad37dc6a4e42ba370
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-24 20:11:52 +0400

    add AddRatingPort

--- src/main/java/com/example/ratingsystem/application/ports/Rating/AddRatingPort.java
+++ src/main/java/com/example/ratingsystem/application/ports/Rating/AddRatingPort.java
@@ -4,6 +4,6 @@ import com.example.ratingsystem.adapters.inbound.DTOs.requests.RatingRequest;
 import com.example.ratingsystem.domain.models.Rating;
 
 public interface AddRatingPort {
-    Rating add(Integer authorId, Integer targetId, RatingRequest ratingRequest);
+    Rating add(String authorEmail, Integer targetId, RatingRequest ratingRequest);
 
 }
